ef9bdab27a79d09372e1d008a160eb98e3180095
sanmuyang <sanmuyang123@163.com>
2018-06-08 17:46:04 +0800

    baidumap改进
    增加了citytocity功能

--- taveler-assistant/baidumap.js
+++ taveler-assistant/baidumap.js
@@ -1,23 +1,34 @@
 ﻿
 /**
- * 参数：
- * 		origin    			起始地
- *      destination 		目的地
- *      origin_region  		起始地所在城市
- *		destination_region  目的地所在城市
- * 返回结果：
- * 		url					百度地图
+ * 参数： 
+ * 		  origin    起始地
+ *        destination  目的地
+ *        origin_region  起始地所在城市
+ *        destination_region  目的地所在城市
+ * 返回结果：  
+ * 		 	百度地图url
+ * 触发条件：
+ *          baidumapAPI为on
+ * 
  * 
  */
 
-
 function baidumap()
 {
-	this.getPost = function (origin,destination,origin_region,destination_region) 
+
+	this.citytocity = function (origin,destination) 
 	{
 
 		var url="http://api.map.baidu.com/direction?origin="+origin+"&destination="+ destination
-				+"&mode=transit"+"&origin_region="+origin_region+"&destination_region="+ destination_region+"&output=html&src=yourCompanyName|yourAppName";
+				+"&mode=transit"+"&origin_region="+origin+"&destination_region="+ destination+"&output=html";
+		return url;
+	}
+	
+	this.origintodestination= function (origin,destination,origin_region,destination_region) 
+	{
+
+		var url="http://api.map.baidu.com/direction?origin="+origin+"&destination="+ destination
+				+"&mode=transit"+"&origin_region="+origin_region+"&destination_region="+ destination_region+"&output=html";
 		return url;
 	}
 
