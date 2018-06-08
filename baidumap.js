cc4d739e4057e9c7c312826a54fdceedf205d89e
sanmuyang <sanmuyang123@163.com>
2018-06-08 16:30:04 +0800

    Add files via upload
    百度地图api

---
+++ baidumap.js
@@ -0,0 +1,27 @@
+﻿
+/**
+ * 参数： origin    起始地
+ *       destination  目的地
+ *       origin_region  起始地所在城市
+ *       destination_region  目的地所在城市
+ * 
+ * 
+ * 返回结果：  百度地图url
+ * 
+ */
+
+
+function baidumap()
+{
+	this.getPost = function (origin,destination,origin_region,destination_region) 
+	{
+
+		var url="http://api.map.baidu.com/direction?origin="+origin+"&destination="+ destination
+				+"&mode=transit"+"&origin_region="+origin_region+"&destination_region="+ destination_region+"&output=html&src=yourCompanyName|yourAppName";
+		return url;
+	}
+
+};
+
+module.exports = baidumap;
+
