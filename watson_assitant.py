f821c6329a529f6296eacf23a6d89d7f5f816f55
Eden1114 <773964676@qq.com>
2018-06-08 17:03:23 +0800

    traveler项目代码
    提交traveler项目代码

--- watson_assitant.py
+++ watson_assitant.py
@@ -1,5 +1,5 @@
 '''
-设置
+credentials:
 {
     "url": "https://gateway.watsonplatform.net/assistant/api",
     "username": "015fa27e-57f2-4b95-a233-0d9116989090",
@@ -10,6 +10,7 @@
 '''Introduction
 The IBM Watson™ Assistant service combines machine learning, natural language understanding, and integrated dialog tools to create conversation flows between your apps and your users.
 '''
+
 from watson_developer_cloud import AssistantV1
 from watson_developer_cloud import WatsonApiException
 import json
@@ -21,9 +22,50 @@ assistant = AssistantV1(
     password='e6TwQQcPoqoe',
     url='https://gateway.watsonplatform.net/assistant/api'
 )
+#########################
+# Workspaces
+#########################
 
-#Exception框架
-#try:
-#    # Invoke a Assistant method
-#except WatsonApiException as ex:
-#    print "Method failed with status code " + str(ex.code) + ": " + ex.message
+create_workspace_data = {
+    "name": "test_workspace",
+    "description": "integration tests",
+    "language": "en",
+    "intents": [{
+        "intent": "hello",
+        "description": "string",
+        "examples": [{
+            "text": "good morning"
+        }]
+    }],
+    "entities": [{
+        "entity": "pizza_toppings",
+        "description": "Tasty pizza toppings",
+        "metadata": {
+            "property": "value"
+        }
+    }],
+    "counterexamples": [{
+        "text": "string"
+    }],
+    "metadata": {},
+}
+
+response = assistant.create_workspace(
+    name=create_workspace_data['name'],
+    description=create_workspace_data['description'],
+    language='en',
+    intents=create_workspace_data['intents'],
+    entities=create_workspace_data['entities'],
+    counterexamples=create_workspace_data['counterexamples'],
+    metadata=create_workspace_data['metadata'])
+print(json.dumps(response, indent=2))
+
+"""
+Exception框架
+
+try:
+    # Invoke a Assistant method
+except WatsonApiException as ex:
+    print "Method failed with status code " + str(ex.code) + ": " + ex.message
+
+"""
