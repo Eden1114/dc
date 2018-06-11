1ebcdd33b758d49227d07d61f7db683b7e399ab2
sanmuyang <sanmuyang123@163.com>
2018-06-11 11:59:11 +0800

    天气api
    node调用心知天气api

---
+++ weather.js
@@ -0,0 +1,42 @@
+/**
+ * 参数： 
+ * 		  location 目标城市，支持拼音和汉语
+ * 返回结果：  
+ * 		 	天气情况string
+ * 触发条件：
+ *      weatherAPI为on
+ */
+
+function weather()
+{
+
+	this.getweather = function (loaction) 
+	{
+
+      const UID = "UD08492217"; // 测试用 用户ID，请更换成您自己的用户ID
+      const KEY = "co5hkpvb2ddse5sg"; // 测试用 key，请更换成您自己的 Key
+      var LOCATION = loaction; // 除拼音外，还可以使用 v3 id、汉语等形式
+
+      var Api = require('./xinzhiapi.js')
+      var argv = require('optimist').default('l', LOCATION).argv;
+
+      var api = new Api(UID, KEY);
+
+      var weather;
+      api.getWeatherNow(argv.l).then(function(data) 
+      {
+        //console.log(JSON.stringify(data, null, 4));
+
+        weather = data.results[0].location.name+"今天天气"+data.results[0].now.text+"，平均气温"+data.results[0].now.temperature+"度";
+        //console.log(weather);
+        return weather;
+        
+      }).catch(function(err) 
+      {
+        console.log(err.error.status);
+      });	
+	}
+};
+
+module.exports = weather;
+
