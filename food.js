fd36b5ff377bc37bb41dc395f7c9269427075eab
sanmuyang <sanmuyang123@163.com>
2018-06-15 00:03:07 +0800

    美食和宾馆api
    改成了直接调起百度地图服务

--- food.js
+++ food.js
@@ -1,55 +1,23 @@
+
 /**
  * 参数： 
- * 		  city 城市
- *        kind 美食类型，例如烧烤，火锅
+ *       kind 美食的种类，如烧烤，火锅等
+ * 		 city 城市
  * 返回结果：  
- * 		  餐馆的string
+ * 		 	百度地图url 
  */
+
 function food()
 {
-	this.getfood = function (city,kind) 
+
+	this.getfood= function (kind,city) 
 	{
-		var request=require('request');
-		var URL="http://api.map.baidu.com/place/v2/search?query="+kind+"&tag=美食&region="+city+"&filter=cater&output=json&ak=hDzuFZ8W3ygA007ogMf4TS9SGwGvpOOy";
-		request.post({ url: URL, form: { key: 'value' } }, function (error, response, data) {
-			if (!error && response.statusCode == 200) 
-			{
-				var dataObj=eval("("+data+")");
-				var food = dataObj.results[0].name;
-				var foodarea = dataObj.results[0].city + dataObj.results[0].area+dataObj.results[0].address;
 
-				var ans = "最受欢迎的美食："+food +", 地点:"+foodarea;
-				return ans;
-			
-			}
-		})
+		var url="http://api.map.baidu.com/place/search?query="+kind+"&radius=1000&region="+city+"&output=html" ;
+		return url;
 	}
-
+	
 };
 
+
 module.exports = food;
-
-
-
-
-
-
-
-
-
-
-/*
-var request=require('request');
-
-request.post({ url: 'http://api.map.baidu.com/place/v2/search?query=烧烤&tag=美食&region=北京&filter=cater&output=json&ak=hDzuFZ8W3ygA007ogMf4TS9SGwGvpOOy', form: { key: 'value' } }, function (error, response, data) {
-			if (!error && response.statusCode == 200) 
-			{
-				var dataObj=eval("("+data+")");
-				var food = dataObj.results[0].name;
-				var foodarea = dataObj.results[0].city + dataObj.results[0].area+dataObj.results[0].address;
-
-				var ans = "最受欢迎的美食："+food +", 地点:"+foodarea;
-			    console.log(ans);
-			}
-		})
-*/
--- hotel.js
+++ hotel.js
@@ -1,53 +1,20 @@
-
-
 /**
  * 参数： 
- * 		  city 城市
+ * 		 city 城市
  * 返回结果：  
- * 		  酒店的string
+ * 		 	百度地图url 
  */
+
 function hotel()
 {
-	this.gethotel = function (city) 
+
+	this.gethotel= function (city) 
 	{
-		var request=require('request');
-		var URL="http://api.map.baidu.com/place/v2/search?query=酒店&region="+city+"&filter=hotel&output=json&ak=hDzuFZ8W3ygA007ogMf4TS9SGwGvpOOy"
 
-		request.post({ url: URL, form: { key: 'value' } }, function (error, response, data) {
-			if (!error && response.statusCode == 200) 
-			{
-				var dataObj=eval("("+data+")");
-				var hotel= dataObj.results[0].name;
-				var hotelarea = dataObj.results[0].city + dataObj.results[0].area+dataObj.results[0].address;
-
-				var ans = "最受欢迎的酒店："+ hotel +", 地点:"+hotelarea;
-			    
-				return ans;
-			
-			}
-		})
+		var url="http://api.map.baidu.com/place/search?query=宾馆&radius=1000&region="+city+"&output=html" ;
+		return url;
 	}
-
+	
 };
 
 module.exports = hotel;
-
-
-
-
-/*
-var request=require('request');
-request.post({ url: 'http://api.map.baidu.com/place/v2/search?query=酒店&region=北京&filter=hotel&output=json&ak=hDzuFZ8W3ygA007ogMf4TS9SGwGvpOOy', form: { key: 'value' } }, function (error, response, data) {
-			if (!error && response.statusCode == 200) 
-			{
-				var dataObj=eval("("+data+")");
-				var hotel= dataObj.results[0].name;
-				var hotelarea = dataObj.results[0].city + dataObj.results[0].area+dataObj.results[0].address;
-
-				var ans = "最受欢迎的酒店："+ hotel +", 地点:"+hotelarea;
-			    
-				console.log(ans);
-			
-			}
-		})
-*/
